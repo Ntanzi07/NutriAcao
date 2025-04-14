d93003a964e8a86e1831a79a0c1f8e148fe292d9
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-04-14 13:40:00 -0300

    fixing the style

--- components/chatbot/UserOptions/UserCalculator.tsx
+++ components/chatbot/UserOptions/UserCalculator.tsx
@@ -98,182 +98,194 @@ const UserCalculator = (props: Props) => {
         decorative
         orientation="horizontal"
       />
-      <div className="flex flex-col gap-5 mt-3 text-[1.1em]">
-        <fieldset>
-          <div className="flex gap-2 w-full">
-            <legend className="font-semibold ">Sexo:</legend>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="sexo"
-                value="masculino"
-                checked={form.sexo === "masculino"}
-                onChange={handleChange}
-              />
-              Masculino
+
+      <div className="flex flex-col gap-2 mt-3 text-[1.1em]">
+
+        <h2 className='font-semibold text-[1.2em]'>Passo 1: Dados Pessoais</h2>
+
+        <div className='flex flex-col gap-2 px-3 rounded-lg'>
+          <fieldset>
+            <div className="flex gap-2 w-full">
+              <legend className="font-semibold ">Sexo:</legend>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="sexo"
+                  value="masculino"
+                  checked={form.sexo === "masculino"}
+                  onChange={handleChange}
+                />
+                Masculino
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="sexo"
+                  value="feminino"
+                  checked={form.sexo === "feminino"}
+                  onChange={handleChange}
+                />
+                Feminino
+              </label>
+            </div>
+          </fieldset>
+
+          <div className='flex justify-between gap-5'>
+            <label className='flex w-fit items-center gap-2'>
+              <legend className="font-semibold ">Peso (kg):</legend>
+              <input name="peso" min={0} max={400} type="number" value={form.peso} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
             </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="sexo"
-                value="feminino"
-                checked={form.sexo === "feminino"}
-                onChange={handleChange}
-              />
-              Feminino
+            <label className='flex items-center gap-2'>
+              <legend className="font-semibold ">Altura (cm):</legend>
+              <input name="altura" min={0} max={260} type="number" value={form.altura} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
+            </label>
+
+            <label className='flex items-center gap-2'>
+              <legend className="font-semibold ">Idade:</legend>
+              <input name="idade" min={0} max={120} type="number" value={form.idade} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
             </label>
           </div>
-        </fieldset>
-
-        <div className='flex justify-between gap-5'>
-          <label className='flex w-fit items-center gap-2'>
-            <legend className="font-semibold ">Peso (kg):</legend>
-            <input name="peso" min={0} max={400} type="number" value={form.peso} onChange={handleChange} className=" bg-transparent border-b-2 border-black" />
-          </label>
-
-          <label className='flex items-center gap-2'>
-            <legend className="font-semibold ">Altura (cm):</legend>
-            <input name="altura" min={0} max={260} type="number" value={form.altura} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
-          </label>
-
-          <label className='flex items-center gap-2'>
-            <legend className="font-semibold ">Idade:</legend>
-            <input name="idade" min={0} max={120} type="number" value={form.idade} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
-          </label>
         </div>
 
-        <fieldset>
-          <legend className="font-semibold">Atividade diária:</legend>
-          <div className="flex gap-2 w-full justify-between pr-2">
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="atividadeDiaria"
-                value={0}
-                checked={form.atividadeDiaria === 0}
-                onChange={(e) => setForm({ ...form, atividadeDiaria: Number(e.target.value) })}
-              />
-              Sedentário
-            </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="atividadeDiaria"
-                value={1}
-                checked={form.atividadeDiaria === 1}
-                onChange={(e) => setForm({ ...form, atividadeDiaria: Number(e.target.value) })}
-              />
-              Moderadamente ativo
-            </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="atividadeDiaria"
-                value={2}
-                checked={form.atividadeDiaria === 2}
-                onChange={(e) => setForm({ ...form, atividadeDiaria: Number(e.target.value) })}
-              />
-              Bastante ativo
-            </label>
-          </div>
-        </fieldset>
+        <h2 className='font-semibold text-[1.2em] mt-5'>Passo 2: Nível de Atividade e Musculação</h2>
 
-        <label className='flex gap-3 justify-between'>
-          <legend className="font-semibold text-wrap">Frequência de musculação (por semana):</legend>
-          <input name="freqMusculacao" min={0} max={28} type="number" value={form.freqMusculacao} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
-        </label>
+        <div className='flex flex-col gap-3 px-3 rounded-lg'>
+          <fieldset>
+            <legend className="font-semibold">Atividade diária:</legend>
+            <div className="flex gap-2 w-full justify-between pr-2">
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="atividadeDiaria"
+                  value={0}
+                  checked={form.atividadeDiaria === 0}
+                  onChange={(e) => setForm({ ...form, atividadeDiaria: Number(e.target.value) })}
+                />
+                Sedentário
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="atividadeDiaria"
+                  value={1}
+                  checked={form.atividadeDiaria === 1}
+                  onChange={(e) => setForm({ ...form, atividadeDiaria: Number(e.target.value) })}
+                />
+                Moderadamente ativo
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="atividadeDiaria"
+                  value={2}
+                  checked={form.atividadeDiaria === 2}
+                  onChange={(e) => setForm({ ...form, atividadeDiaria: Number(e.target.value) })}
+                />
+                Bastante ativo
+              </label>
+            </div>
+          </fieldset>
+          <label className='flex gap-3 justify-between'>
+            <legend className=" text-wrap">Frequência de musculação (por semana):</legend>
+            <input name="freqMusculacao" min={0} max={28} type="number" value={form.freqMusculacao} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
+          </label>
 
-        <label className='flex gap-3 justify-between'>
-          <legend className="font-semibold text-wrap">Duração média do treino de musculação (min):</legend>
-          <input name="duracaoMusculacao" min={0} max={1000} type="number" value={form.duracaoMusculacao} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
-        </label>
+          <label className='flex gap-3 justify-between'>
+            <legend className=" text-wrap">Duração média do treino de musculação (min):</legend>
+            <input name="duracaoMusculacao" min={0} max={1000} type="number" value={form.duracaoMusculacao} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
+          </label>
 
-        <fieldset>
-          <div className="flex gap-2 w-full justify-between pr-2">
-            <legend className="font-semibold">Intensidade da musculação:</legend>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="intensidadeMusculacao"
-                value={0}
-                checked={form.intensidadeMusculacao === 0}
-                onChange={(e) => setForm({ ...form, intensidadeMusculacao: Number(e.target.value) })}
-              />
-              Leve
-            </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="intensidadeMusculacao"
-                value={1}
-                checked={form.intensidadeMusculacao === 1}
-                onChange={(e) => setForm({ ...form, intensidadeMusculacao: Number(e.target.value) })}
-              />
-              Moderada
-            </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="intensidadeMusculacao"
-                value={2}
-                checked={form.intensidadeMusculacao === 2}
-                onChange={(e) => setForm({ ...form, intensidadeMusculacao: Number(e.target.value) })}
-              />
-              Alta
-            </label>
-          </div>
-        </fieldset>
+          <fieldset>
+            <div className="flex gap-2 w-full justify-between pr-2">
+              <legend className="">Intensidade da musculação:</legend>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="intensidadeMusculacao"
+                  value={0}
+                  checked={form.intensidadeMusculacao === 0}
+                  onChange={(e) => setForm({ ...form, intensidadeMusculacao: Number(e.target.value) })}
+                />
+                Leve
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="intensidadeMusculacao"
+                  value={1}
+                  checked={form.intensidadeMusculacao === 1}
+                  onChange={(e) => setForm({ ...form, intensidadeMusculacao: Number(e.target.value) })}
+                />
+                Moderada
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="intensidadeMusculacao"
+                  value={2}
+                  checked={form.intensidadeMusculacao === 2}
+                  onChange={(e) => setForm({ ...form, intensidadeMusculacao: Number(e.target.value) })}
+                />
+                Alta
+              </label>
+            </div>
+          </fieldset>
+        </div>
 
-        <label className='flex gap-3 justify-between'>
-          <legend className="font-semibold text-wrap">Frequência de aeróbicos (por semana):</legend>
-          <input name="freqAerobico" min={0} max={28} type="number" value={form.freqAerobico} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
-        </label>
+        <h2 className='font-semibold text-[1.2em] mt-5'>Passo 3: Exercícios Aeróbicos</h2>
 
-        <label className='flex gap-3 justify-between'>
-          <legend className="font-semibold text-wrap">Duração média dos aeróbicos (min):</legend>
-          <input name="duracaoAerobico" min={0} max={1000} type="number" value={form.duracaoAerobico} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
-        </label>
+        <div className='flex flex-col gap-3 px-3 rounded-lg'>
+          <label className='flex gap-3 justify-between'>
+            <legend className="text-wrap">Frequência de aeróbicos (por semana):</legend>
+            <input name="freqAerobico" min={0} max={28} type="number" value={form.freqAerobico} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
+          </label>
 
-        <fieldset>
-          <div className="flex gap-2 w-full justify-between pr-2">
-            <legend className="font-semibold">Intensidade dos aeróbicos:</legend>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="intensidadeAerobico"
-                value={0}
-                checked={form.intensidadeAerobico === 0}
-                onChange={(e) => setForm({ ...form, intensidadeAerobico: Number(e.target.value) })}
-              />
-              Leve
-            </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="intensidadeAerobico"
-                value={1}
-                checked={form.intensidadeAerobico === 1}
-                onChange={(e) => setForm({ ...form, intensidadeAerobico: Number(e.target.value) })}
-              />
-              Moderada
-            </label>
-            <label className="flex items-center gap-1">
-              <input
-                type="radio"
-                name="intensidadeAerobico"
-                value={2}
-                checked={form.intensidadeAerobico === 2}
-                onChange={(e) => setForm({ ...form, intensidadeAerobico: Number(e.target.value) })}
-              />
-              Alta
-            </label>
-          </div>
-        </fieldset>
+          <label className='flex gap-3 justify-between'>
+            <legend className="text-wrap">Duração média dos aeróbicos (min):</legend>
+            <input name="duracaoAerobico" min={0} max={1000} type="number" value={form.duracaoAerobico} onChange={handleChange} className="w-[75px] bg-transparent border-b-2 border-black" />
+          </label>
+
+          <fieldset>
+            <div className="flex gap-2 w-full justify-between pr-2">
+              <legend className="">Intensidade dos aeróbicos:</legend>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="intensidadeAerobico"
+                  value={0}
+                  checked={form.intensidadeAerobico === 0}
+                  onChange={(e) => setForm({ ...form, intensidadeAerobico: Number(e.target.value) })}
+                />
+                Leve
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="intensidadeAerobico"
+                  value={1}
+                  checked={form.intensidadeAerobico === 1}
+                  onChange={(e) => setForm({ ...form, intensidadeAerobico: Number(e.target.value) })}
+                />
+                Moderada
+              </label>
+              <label className="flex items-center gap-1">
+                <input
+                  type="radio"
+                  name="intensidadeAerobico"
+                  value={2}
+                  checked={form.intensidadeAerobico === 2}
+                  onChange={(e) => setForm({ ...form, intensidadeAerobico: Number(e.target.value) })}
+                />
+                Alta
+              </label>
+            </div>
+          </fieldset>
+        </div>
       </div>
 
       <button
         onClick={calcularTDEE}
-        className="mt-6 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+        className="mt-6 bg-primary-green text-white px-4 py-2 rounded hover:bg-blue-700"
       >
         Calcular TDEE
       </button>

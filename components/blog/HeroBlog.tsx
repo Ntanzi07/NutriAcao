b12d49e9353122116fb4bbf183e0092d93635efc
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-05-04 12:03:12 -0300

    small change in heroBlog

--- components/blog/HeroBlog.tsx
+++ components/blog/HeroBlog.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const HeroBlog = (props: Props) => {
   return (
-    <div className="relative h-[50vh] top-0">
+    <div className="relative mt-[3em] h-[65vh]">
       <Image src="/blog.jpg" alt="blog" fill className="object-cover" />
       <div className="absolute inset-0 flex items-center justify-center">
         <h2 className="text-center text-white text-[10em] tracking-widest">Our <i>Blog.</i></h2>
